53b9056614a84cd53a8706bb3f1ff740f55ffab4
mirkoalicastro <mirkoalicastro@gmail.com>
2021-02-27 12:28:39 +0100

    Adds curly braces to if-else

--- src/main/kotlin/com/mirkoalicastro/mavenversionrefactor/domain/Pom.kt
+++ src/main/kotlin/com/mirkoalicastro/mavenversionrefactor/domain/Pom.kt
@@ -2,10 +2,4 @@ package com.mirkoalicastro.mavenversionrefactor.domain
 
 import com.intellij.psi.xml.XmlTag
 
-data class Pom(val namespace: String, val project: XmlTag, val xmlDependency: XmlDependency) {
-    companion object {
-        private const val MAVEN_NAMESPACE = "^(http://maven\\.apache\\.org).*$"
-    }
-
-    fun hasValidNamespace() = namespace.matches(Regex(MAVEN_NAMESPACE))
-}
+data class Pom(val project: XmlTag, val xmlDependency: XmlDependency)
