81439d0f3b4b40c55fb13d720d27fd5114cabd10
Mirko Alicastro <mirkoalicastro@gmail.com>
2021-02-27 12:30:12 +0100

    Finalize latest version (#14)
    * Updates detekt configuration to be consisent with ktlint
    
    * Removes deprecate API usage
    
    * Adds curly braces to if-else

--- src/main/kotlin/com/mirkoalicastro/mavenversionrefactor/factory/PomFactory.kt
+++ src/main/kotlin/com/mirkoalicastro/mavenversionrefactor/factory/PomFactory.kt
@@ -22,13 +22,11 @@ class PomFactory {
     }
 
     fun create(currentPsiElement: PsiElement) =
-        getXmlTokenForPom(currentPsiElement)?.let { create(it) }
-
-    private fun getXmlTokenForPom(psiElement: PsiElement) =
-        if (psiElement.containingFile.name.equals(POM_FILE_NAME, ignoreCase = true))
-            castOrNull(psiElement, XmlToken::class.java)
-        else
+        if (currentPsiElement.containingFile.name.equals(POM_FILE_NAME, ignoreCase = true)) {
+            castOrNull(currentPsiElement, XmlToken::class.java)?.let { create(it) }
+        } else {
             null
+        }
 
     private fun create(currentXmlToken: XmlToken): Pom? {
         val (versionText, versionTag) = extractVersionTextAndTag(currentXmlToken)
@@ -54,7 +52,7 @@ class PomFactory {
     private fun createPom(version: XmlText, versionTag: XmlTag): Pom? {
         val isVersionTag = VERSION.xmlName == versionTag.name
         val dependency = castOrNull(versionTag.parent, XmlTag::class.java)
-        return if (isVersionTag && dependency != null && !isVariable(version.text)) {
+        return if (isVersionTag && dependency != null && !version.text.matches(Regex(POM_VARIABLE_PATTERN))) {
             createPom(dependency, version)
         } else {
             null
@@ -64,7 +62,9 @@ class PomFactory {
     private fun createPom(dependency: XmlTag, version: XmlText): Pom? {
         val project = findBack(dependency, PROJECT.xmlName)
         return if (project != null && (dependency.name == DEPENDENCY.xmlName || dependency.name == PLUGIN.xmlName)) {
-            createPom(project, dependency, version)
+            createDependency(dependency, version.text)?.let {
+                Pom(project, XmlDependency(it, version))
+            }
         } else {
             null
         }
@@ -79,17 +79,12 @@ class PomFactory {
             }
         }
 
-    private fun createPom(project: XmlTag, dependencyTag: XmlTag, versionText: XmlText) =
-        createDependency(dependencyTag, versionText.text)?.let {
-            Pom(dependencyTag.namespace, project, XmlDependency(it, versionText))
-        }
-
     private fun createDependency(dependencyTag: XmlTag, version: String): Dependency? {
         val xmlTagChildren = extractXmlElement(dependencyTag, XmlTag::class.java)
         val groupIdTag = extractXmlTagByName(xmlTagChildren, GROUP_ID.xmlName)
         val artifactIdTag = extractXmlTagByName(xmlTagChildren, ARTIFACT_ID.xmlName)
         return if (groupIdTag != null && artifactIdTag != null) {
-            createDependency(version, groupIdTag, artifactIdTag)
+            Dependency(groupIdTag.value.text, artifactIdTag.value.text, version)
         } else {
             null
         }
@@ -100,12 +95,6 @@ class PomFactory {
             ?.filter { clazz.isInstance(it) }
             ?.map { clazz.cast(it) }
             ?: emptyList()
-
-    private fun createDependency(version: String, groupIdTag: XmlTag, artifactIdTag: XmlTag) =
-        Dependency(groupIdTag.value.text, artifactIdTag.value.text, version)
-
-    private fun isVariable(version: String) =
-        version.matches(Regex(POM_VARIABLE_PATTERN))
 
     private fun extractXmlTagByName(xmlTagChildren: List<XmlTag>, name: String) =
         xmlTagChildren.firstOrNull { name == it.name }
